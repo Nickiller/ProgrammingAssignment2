db985e17277855154992f36311825b083c1eff74
Nick Zhang <zhangxy.pro@gmail.com>
2014-05-03 17:27:12 +0800

    write the two functions to return and cache inversed matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## i, initialized as a matrix of NAs, is used to cache the inverse of x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- matrix(NA,nrow(x),ncol(x))
+    set <- function(y){
+        x <<- y
+        i <<- matrix(NA,nrow(x),ncol(x))
+    }
+    get <- function() x
+    setInverse <- function(inversed) i <<- inversed
+    getInverse <- function() i
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if (!is.na(i[1,1])){
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setInverse(i)
+    i
 }
